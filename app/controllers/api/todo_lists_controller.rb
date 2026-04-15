ac0d854d351978a8fb730f2589bcb03bd75cbc25
Diego k <diegoklappenbach@hotmail.com>
2026-04-15 18:45:45 -0300

    Add actions to todo list api controller

--- app/controllers/api/todo_lists_controller.rb
+++ app/controllers/api/todo_lists_controller.rb
@@ -1,10 +1,63 @@
 module Api
   class TodoListsController < ApplicationController
+    protect_from_forgery with: :null_session
+
     # GET /api/todolists
     def index
       @todo_lists = TodoList.all
 
-      respond_to :json
+      render json: @todo_lists
     end
+
+    # POST /api/todolists
+    def create
+      @todo_list = TodoList.new(todo_list_params)
+
+      ActiveRecord::Base.transaction do
+        @todo_list.save!
+
+        params[:items]&.each do |item|
+          @todo_list.todo_items.create!(description: item[:description], completed: item[:completed])
+        end
+      end
+
+      render json: @todo_list.as_json(include: :todo_items), status: :created
+    rescue ActiveRecord::RecordInvalid => e
+      render json: { error: e.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+    # PATCH/PUT /api/todolists/:id
+    def update
+      todo_list = TodoList.find_by(id: params[:id])
+
+      if todo_list
+        if todo_list.update(todo_list_params)
+          render json: todo_list, status: :ok
+        else
+          render json: { error: todo_list.errors.full_messages }, status: :unprocessable_entity
+        end
+      else
+        render json: { error: "Todo list not found" }, status: :not_found
+      end
+    end
+
+    # DELETE /api/todolists/:id
+    def destroy
+      todo_list = TodoList.find_by(id: params[:id])
+
+      if todo_list
+        todo_list.destroy
+        render json: { success: "Todo list deleted" }, status: :ok
+      else
+        render json: { error: "Todo list not found" }, status: :not_found
+      end
+    end
+
+    private
+
+    def todo_list_params
+      params.require(:todo_list).permit(:name)
+    end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   mount Sidekiq::Web => "/sidekiq"
   root "todo_lists#index"
   namespace :api do
-    resources :todo_lists, only: %i[index], path: :todolists do
+    resources :todo_lists, only: %i[index create update destroy], path: :todolists do
       resources :todo_items, only: %i[index create show update destroy], path: :todoitems do
         post 'complete_all', on: :collection
       end
